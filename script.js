a33ba20da303f672a25e416e7e8b77e26f75a074
boy1bawang <71049649+boy1bawang@users.noreply.github.com>
2025-01-11 20:52:33 +0800

    Add files via upload
    foundation

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+let score = 0;
+const clickButton = document.getElementById('clickButton');
+const scoreDisplay = document.getElementById('score');
+const popup = document.createElement('div');
+popup.id = 'popup';
+popup.textContent = 'I Love Youuuuuuu, hehe';
+document.body.appendChild(popup);
+
+clickButton.addEventListener('click', () => {
+    score++;
+    scoreDisplay.textContent = score;
+
+    if (score === 100) {
+        popup.style.display = 'block';
+    }
+});
